d30e612948bd7c48571af36e2e1ef24a0565ead6
Endre Stølsvik <endre@stolsvik.com>
2023-04-13 00:09:36 +0200

    Added the ActiveMqMinimal.java used in the tutorial

---
+++ jbang/ActiveMqMinimal.java
@@ -0,0 +1,15 @@
+//usr/bin/env jbang "$0" "$@" ; exit $?
+//JAVA 17
+//DEPS io.mats3.examples:mats-jbangkit:RC1-1.0.0
+
+import io.mats3.examples.jbang.MatsJbangKit;
+import io.mats3.test.broker.MatsTestBroker;
+import io.mats3.test.broker.MatsTestBroker.ActiveMq;
+
+public class ActiveMqMinimal {
+    public static void main(String[] args) {
+        MatsJbangKit.configureLogbackToConsole_Info();
+        MatsTestBroker.newActiveMqBroker(ActiveMq.LOCALHOST, ActiveMq.SHUTDOWNHOOK)
+                .waitUntilStopped();
+    }
+}
